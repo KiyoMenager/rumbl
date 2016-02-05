43cacde7562178233fe799ebf7028c9fdd78ed5a
Kiyoaki Menager <menager.kiyoaki@gmail.com>
2016-02-05 06:04:56 +0100

    initial commit

---
+++ web/controllers/session_controller.ex
@@ -0,0 +1,30 @@
+defmodule Rumbl.SessionController do
+	use Rumbl.Web, :controller
+
+	alias Rumbl.Auth
+	alias Rumbl.Repo
+
+	def new(conn, _) do
+		render conn, "new.html"
+	end
+	
+	def create(conn, %{"session" => %{"username" => user, "password" => pass }}) do
+		case Auth.login_by_username_and_pass(conn, user, pass, repo: Repo) do
+			{:ok, conn} ->
+				conn
+				|> put_flash(:info, "Welcome back!")
+				|> redirect(to: page_path(conn, :index))
+			{:error, _reason, conn} ->
+				conn
+				|> put_flash(:error, "Invalid username/password combination")
+				|> render("new.html")
+		end
+	end
+
+	def delete(conn, _) do
+		conn
+		|> Auth.logout()
+		|> put_flash(:info, "You have been logged out")
+		|> redirect(to: page_path(conn, :index))
+	end
+end
